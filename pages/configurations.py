691f667a7fd289c09d03e4bbd5cb81cebe6a8d80
Pedro Cunha <idk.pedroc@gmail.com>
2024-03-20 03:57:27 -0300

    dealing with perserverance of configurations

--- pages/configurations.py
+++ pages/configurations.py
@@ -10,18 +10,25 @@ class Configurations(BasePage):
     def draw(self) -> None:
         cols = self.st.columns(3, gap="large")
         
+        max_h = 0.0 if "max_value_humidity" not in self.st.session_state else self.st.session_state["max_value_humidity"]
+        min_h = 0.0 if "min_value_humidity" not in self.st.session_state else self.st.session_state["min_value_humidity"]
+        min_t = 0.0 if "min_value_temperature" not in self.st.session_state else self.st.session_state["min_value_temperature"]
+        max_t = 0.0 if "max_value_temperature" not in self.st.session_state else self.st.session_state["max_value_temperature"]
+        min_b = 0.0 if "min_value_brightness" not in self.st.session_state else self.st.session_state["min_value_brightness"]
+        max_b = 0.0 if "max_value_brightness" not in self.st.session_state else self.st.session_state["max_value_brightness"]
+        
         with cols[0]:
             self.st.header("Humidity sensor (%)")
-            min_value_humidity = self.st.number_input("Minimum value of humidity", value=0.0, step=0.01, min_value=0.0, max_value= 100.0)
-            max_value_humidity = self.st.number_input("Maximum value of humidity", value=100.0, step=0.01,max_value=100.0, min_value= 0.0)
+            min_value_humidity = self.st.number_input("Minimum value of humidity", value=min_h, step=0.01, min_value=0.0, max_value= 100.0)
+            max_value_humidity = self.st.number_input("Maximum value of humidity", value=max_h, step=0.01,max_value=100.0, min_value= 0.0)
         with cols[1]:
             self.st.header("Temperature sensor (°C)")
-            min_value_temperature = self.st.number_input("Minimum value of temperature", value=0.0, step=0.01)
-            max_value_temperature = self.st.number_input("Maximum value of temperature", value=100.0, step=0.01)
+            min_value_temperature = self.st.number_input("Minimum value of temperature", value=min_t, step=0.01)
+            max_value_temperature = self.st.number_input("Maximum value of temperature", value=max_t, step=0.01)
         with cols[2]:
             self.st.header("Brightness sensor (%)")
-            min_value_brightness = self.st.number_input("Minimum value of brightness", value=0.0, step=0.01, min_value=0.0, max_value=100.0)
-            max_value_brightness = self.st.number_input("Maximum value of brightness", value=100.0, step=0.01,max_value=100.0, min_value=0.0)
+            min_value_brightness = self.st.number_input("Minimum value of brightness", value=min_b, step=0.01, min_value=0.0, max_value=100.0)
+            max_value_brightness = self.st.number_input("Maximum value of brightness", value=max_b, step=0.01,max_value=100.0, min_value=0.0)
             
         if self.st.button("Save"):
             variables = {
